707e0e0f7fb8d483831a3acaa317bde04bcc2563
Alberto <albertoperezdelrio@gmail.com>
2025-09-23 14:09:06 +0200

    actualiza título

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ambientales Vault",
+    pageTitle: "🌿Ambientales \nVault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
